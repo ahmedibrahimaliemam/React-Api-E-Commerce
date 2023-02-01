0942d2ad41372d098747595598857656d743991f
ahmedibrahimaliemam <ahmedibrahimaliemam@gmail.com>
2023-02-01 15:14:53 +0200

    delete unimportant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React ,{Component} from "react";
+import React from "react";
 import { BrowserRouter , Route, Routes } from "react-router-dom";
-import { useState , useEffect} from "react";
 import Navbar from "./Component/Navbar";
 import Slider from "./Component/Slider";
 import Productlist from "./Component/Productlist";
 import About from "./Component/About";
 import ProductDetails from "./Component/productDetails";
-import Product from "./Component/Product";
 function App(){
   return(
     <BrowserRouter>
@@ -15,7 +13,7 @@ function App(){
         <Route path="" element={<><Navbar/><Slider/><Productlist/></>}/>
         <Route path="/" element={<><Slider/><Productlist/></>}/>
         <Route path="/About" element={<><About/></>} />
-        <Route path="/product/:productId" element={<ProductDetails/>} />
+        <Route path="/product/:productId" element={<><Navbar/><ProductDetails/></>} />
       </Routes>
       
 
